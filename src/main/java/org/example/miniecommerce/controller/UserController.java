0ee4ab914ab98bcfe38385daac0254948255533c
NguyenChanhKhue <chanhkhue7122005@gmail.com>
2025-10-18 00:12:49 +0700

    3.Xu ly phan Login , Register

--- src/main/java/org/example/miniecommerce/controller/UserController.java
+++ src/main/java/org/example/miniecommerce/controller/UserController.java
@@ -7,6 +7,7 @@ import org.example.miniecommerce.dto.user.UpdateUserRequest;
 import org.example.miniecommerce.dto.user.UserResponse;
 import org.example.miniecommerce.entity.User;
 import org.example.miniecommerce.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -35,16 +37,25 @@ public class UserController {
     return userService.getAllUser();
   }
 
+  @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser(HttpSession session) {
+        Object user = session.getAttribute("user");
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("{\"message\": \"User not logged in\"}");
+        }
+        return ResponseEntity.ok(user);
+      }
 
   @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable String id) {
         return ResponseEntity.ok(userService.getUserId(id));
     }
 
-  @PostMapping
-  public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
-      return ResponseEntity.ok(userService.createUser(request));
-  }
+  // @PostMapping
+  // public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
+  //     return ResponseEntity.ok(userService.createUser(request));
+  // }
 
   @PutMapping("/{id}")
   public ResponseEntity<UserResponse> updateUser(
